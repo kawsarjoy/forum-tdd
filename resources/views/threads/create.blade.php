8efa39363476826325c69a0b255523d8a6f073fb
Mohammad Abu Kawsar <engabukawsar@gmail.com>
2018-09-30 00:51:10 +0600

    validation errors and old data

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -12,17 +12,41 @@
                         {{ csrf_field() }}
                     
                         <div class="form-group">
-                            <label for="title" class="form-control">Title:</label>
-                            <textarea name="title" class="form-control"></textarea>
+                            <label for="channel_id" class="form-label">Choose a channel:</label>
+                            <select name="channel_id" class="form-control" required>
+
+                                <option value="">Choose one...</option>
+                                @foreach(App\Channel::all() as $channel)
+
+                                    <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+
+                                @endforeach
+                            </select>
                         </div>
 
                         <div class="form-group">
-                            <label for="body" class="form-control">Body:</label>
-                            <textarea name="body" rows="8" class="form-control"></textarea>
+                            <label for="title" class="form-label">Title:</label>
+                            <input type="text" name="title" value="{{ old('title') }}" class="form-control" required>
                         </div>
 
+                        <div class="form-group">
+                            <label for="body" class="form-label">Body:</label>
+                            <textarea name="body" rows="8" class="form-control" required>{{ old('body') }}</textarea>
+                        </div>
+                        
+                        <div class="form-group">
                         <button type="submit" class="btn btn-primary">Publish</button>
+                        </div>
+                        @if(count($errors))
+                        <ul class="alert alert-danger">
+                            @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
                     </form>
+
+
                 </div>
             </div>
         </div>
