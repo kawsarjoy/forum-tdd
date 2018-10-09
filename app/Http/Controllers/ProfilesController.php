aa15d37de60b573214a72d654ce6c59b1bebe93f
Mohammad Abu Kawsar <engabukawsar@gmail.com>
2018-10-09 12:58:46 +0600

    extract a controller query to model

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,28 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Activity;
 
 class ProfilesController extends Controller
 {
     public function show(User $user)
     {
-
         return view('profiles.show')->with([
             'profileUser' => $user,
-            'activities'  => $this->getActivity($user)
+            'activities'  => Activity::feed($user)
             ]);
     }
 
-    protected function getActivity(User $user)
-    {
-        return $user->activity()
-                    ->latest()
-                    ->with('subject')
-                    ->take(50)
-                    ->get()
-                    ->groupBy(function($activity){
-
-            return $activity->created_at->format('Y-m-d');
-        });
-    }
 }
