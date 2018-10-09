8bfd6b1038a0a597e3b1b77cef40b869093b2a29
Mohammad Abu Kawsar <engabukawsar@gmail.com>
2018-10-09 11:54:08 +0600

    How to construct an activity feed with TDD2

--- app/Reply.php
+++ app/Reply.php
@@ -19,4 +19,9 @@ class Reply extends Model
         return $this->belongsTo('App\User', 'user_id');
     }
 
+    public function thread()
+    {
+        return $this->belongsTo('App\Thread');
+    }
+
 }
