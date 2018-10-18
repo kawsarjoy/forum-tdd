1356208d368939398163ff9eba5c6b6a5345d10a
Mohammad Abu Kawsar <engabukawsar@gmail.com>
2018-10-19 03:54:00 +0600

    thread subscription part 2

---
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SubscribeToThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function a_user_can_subscribe_to_threads()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $this->post($thread->path() . '/subscriptions');
+
+        $thread->addReply([
+            
+            'user_id' => auth()->id(),
+            'body' => 'some reply'
+        ]);
+
+        $this->assertCount(1, $thread->subscriptions);
+    }
+}
