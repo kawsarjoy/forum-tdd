7b47a8d6a489d4389d1c15d6b1703b991255afd8
Mohammad Abu Kawsar <engabukawsar@gmail.com>
2018-11-21 10:42:47 +0600

    graduating inspection methods

--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -5,7 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use App\Spam;
+use App\Inspections\Spam;
 
 class SpamTest extends TestCase
 {
@@ -13,11 +13,25 @@ class SpamTest extends TestCase
     //use DatabaseMigrations;
     
     /** @test */
-    public function it_validates_spam()
+    public function it_checks_for_invalid_keywords()
     {
         $spam = new Spam();
 
 
         $this->assertFalse($spam->detect('Innocent Reply'));
+
+        $this->expectException('Exception');
+
+        $spam->detect('yahoo customer support');
+    }
+
+    /** @test */
+    public function it_checks_for_any_key_held_down()
+    {
+        $spam = new Spam();
+        
+        $this->expectException('Exception');
+
+        $spam->detect('fsdffffffffffffffffffff');
     }
 }
